19acc8993b64a1c718e5f49b6024103aceb07f7c
Christopher Bruns <cmbruns@rotatingpenguin.com>
2015-09-26 12:12:31 -0400

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
-import os
+#!/bin/env python
+
 from setuptools import setup
-
-from distutils.core import setup
-from distutils.extension import Extension
-
-import ovr.internal
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "pyovr",
     version = "0.7.1000",
     author = "Christopher Bruns",
     author_email = "cmbruns@rotatingpenguin.com",
-    description = ("ctypes-based Python bindings for the Oculus Rift SDK (libOVR)"),
+    description = ("Oculus Rift SDK (libOVR) bindings using ctypes"),
     keywords = "ovr oculus rift",
     url = "https://github.com/cmbruns/pyovr",
     packages=['ovr'],
-    ],
 )
