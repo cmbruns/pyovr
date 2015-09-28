c7ec8c56d2ed4a627a2dd296e8fc45f2199cac06
Christopher Bruns <brunsc@janelia.hhmi.org>
2015-09-28 12:42:47 -0400

    Begin work on automated python wrapper generator "translate_header.pl"

---
+++ ovr/translate_header.pl
@@ -0,0 +1,210 @@
+#!/bin/env perl
+
+use warnings;
+use strict;
+use File::Basename;
+
+# 1) Edit the following line to reflect the location of the OVR include files on your system
+my $include_folder = "C:/Users/brunsc/Documents/ovr_sdk_win_0.7.0.0/OculusSDK/LibOVR/Include";
+
+# 2) Edit this list to change the set of header files to translate
+my @header_files = (
+    "OVR_ErrorCode.h",
+    "OVR_CAPI_0_7_0.h",
+    "OVR_CAPI_GL.h",
+    "Extras/OVR_CAPI_Util.h",
+);
+
+# No more modifications should be necessary...
+
+my $type_rx = '\S[^;\n()]*\S+'; # can have spaces, e.g. "const unsigned int"
+my $ident_rx = '[a-zA-Z][a-zA-Z0-9_]*';
+
+
+translate_header();
+
+
+sub translate_header {
+    open my $fh, ">", "translated.py" or die;
+
+    print $fh <<'END_PREAMBLE';
+"""
+Python module "ovr"
+Python bindings for Oculus Rift SDK version 0.7.0
+
+Works on Windows only at the moment (just like Oculus Rift SDK...)
+"""
+
+import ctypes
+from ctypes import byref
+import sys
+import textwrap
+import math
+
+# Load Oculus runtime library
+# Assumes Oculus Runtime 0.7 install on 32 bit windows
+try:
+    libovr = ctypes.cdll.LibOVRRT32_0_7
+except:
+    print "Is Oculus Runtime 0.7 installed on this Windows machine?"
+    raise
+
+
+ENUM_TYPE = ctypes.c_uint32 # Hopefully a close enough guess...
+
+
+END_PREAMBLE
+
+    process_headers($fh);
+
+    print $fh <<'END_FOOTER';
+# Run test program
+if __name__ == "__main__":
+    # Transcribed from initial example at 
+    # https://developer.oculus.com/documentation/pcsdk/latest/concepts/dg-sensor/
+    initialize(None)
+    hmd, luid = create()
+    desc = getHmdDesc(hmd)
+    print desc.Resolution
+    print desc.ProductName
+    # Start the sensor which provides the Rift's pose and motion.
+    configureTracking(hmd, 
+        TrackingCap_Orientation | # requested capabilities
+        TrackingCap_MagYawCorrection |
+        TrackingCap_Position, 
+        0) # required capabilities
+    # Query the HMD for the current tracking state.
+    ts  = getTrackingState(hmd, getTimeInSeconds())
+    if ts.StatusFlags & (Status_OrientationTracked | Status_PositionTracked):
+        pose = ts.HeadPose
+        print pose.ThePose
+        # TODO:
+
+    destroy(hmd)
+    shutdown()
+END_FOOTER
+
+    close $fh;
+}
+
+sub process_headers {
+    my $fh = shift;
+    foreach my $inc (@header_files) {
+        my $h = join("/", $include_folder, $inc);
+        process_header($h, $fh);
+    }
+}
+
+sub process_header {
+    my $header = shift;
+    my $out = shift;
+    print "Processing $header...\n";
+    die "ERROR: $header does not exist\n" unless -e $header;
+
+    my $short_header_name = basename($header);
+
+    print $out "### BEGIN Declarations from C header file $short_header_name ###\n\n\n";
+
+    # TODO:
+    my $header_string = do {
+        local $/ = undef;
+        open my $fhh, "<", $header
+            or die "could not open $header: $!";
+        <$fhh>;
+    };
+    # print length $header_string, " characters read\n";
+
+    process_code_block($header_string, $out, $short_header_name);
+
+    print $out "### END Declarations from C header file $short_header_name ###\n\n\n";    
+}
+
+sub process_code_block {
+    my $code = shift;
+    my $out = shift;
+    my $fname = shift;
+
+    # Store offsets of code lines, so we can later translate positions to line numbers
+    my @line_for_pos = ();
+    push @line_for_pos, 1;
+    my $line_count = 0;
+    while ($code =~ m/(\n)/g) {
+        push @line_for_pos, pos($code) + 1;
+        $line_count += 1;
+    }
+    # print "$line_count lines found\n";
+
+    my %translated_by_pos = ();
+
+    process_simple_typedefs($code, \%translated_by_pos);
+
+    my $line_number = 0;
+    foreach my $pos (sort keys %translated_by_pos) {
+        while ($line_for_pos[$line_number] < $pos) {
+            $line_number += 1;
+        }
+        print $out "# Translated from header file $fname:$line_number\n";
+        print $out $translated_by_pos{$pos}, "\n\n";
+    }
+}
+
+sub process_simple_typedefs {
+    my $code = shift;
+    my $by_pos = shift;
+
+    # First use a simple regex, to be sure of counting all examples
+    # "typedef int32_t ovrResult;"
+    my $count1 = 0;
+    # Look for typedefs with a semicolon on the same line
+    while ($code =~ m/(typedef[^\n\/()]*;)/g) {
+        $count1 += 1;
+    }
+
+    my $count2 = 0;    
+    while ($code =~ m/(typedef[ \t]+($type_rx)[ \t]+($ident_rx)[ \t]*;)/g) {
+        my $type = $2;
+        my $ident = $3;
+        my $p = pos($code);
+        $count2 += 1;
+
+        my $tid = translate_type($ident); # Yes, translate_type, not translate_ident
+        my $ttype = translate_type($type);
+        my $trans = "$tid = $ttype\n";
+        print $trans;
+        $by_pos->{$p} = $trans;
+    }
+    # print "$count1 ($count2) simple typedefs found";
+}
+
+sub translate_ident {
+    my $id0 = shift;
+    # TODO
+    return $id0;
+}
+
+sub translate_type {
+    my $type = shift;
+    # remove const
+    $type =~ s/\bconst\s+//g;
+    $type =~ s/\s+const\b//g;
+    # remove struct
+    $type =~ s/\bstruct\s+//g;
+    # no implicit int
+    $type = "unsigned int" if $type =~ m/^\bunsigned\b$/;
+    # abbreviate type
+    $type =~ s/_t\b//g; # int32_t => int32
+    $type =~ s/\bunsigned\s+/u/g; # unsigned int => uint
+    # translate to ctypes type name
+    if ($type =~ m/^(float|u?int|double|u?char|u?short|u?long)/) {
+        $type = "ctypes.c_$type";
+    }
+    # remove leading "ovr" or "ovr_"
+    if ($type =~ m/^ovr_?(.*)$/) {
+        $type = ucfirst($1); # capitalize first letter of types
+    }
+    # translate pointer type
+    while ($type =~ m/^([^\*]+)\*(.*)$/) { # HmdStruct* -> ctypes.POINTER(HmdStruct)
+       $type = "ctypes.POINTER($1)$2";
+    }
+    return $type;
+}
