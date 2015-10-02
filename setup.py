859c57b3efa5ebe1e2ec7eecea963ee7c382d4e7
Christopher Bruns <cmbruns@rotatingpenguin.com>
2015-10-02 15:28:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@
 
 from setuptools import setup
 
+_PYOVR_VERSION = "0.7.1000"
+
 setup(
     name = "pyovr",
-    version = "0.7.1000",
+    version = _PYOVR_VERSION,
     author = "Christopher Bruns",
     author_email = "cmbruns@rotatingpenguin.com",
-    description = ("Oculus Rift SDK (libOVR) bindings using ctypes"),
-    keywords = "ovr oculus rift",
+    description = "Oculus Rift SDK (libOVR) bindings using ctypes",
     url = "https://github.com/cmbruns/pyovr",
+    download_url = "https://github.com/cmbruns/pyovr/tarball/" + _PYOVR_VERSION,
     packages=['ovr'],
+    keywords = "ovr oculus rift",
+    classifiers = [],
 )
