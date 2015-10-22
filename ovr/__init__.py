1af090db622101d79cb01a40c53d5a06eb072f60
Christopher Bruns <brunsc@janelia.hhmi.org>
2015-10-22 14:08:06 -0400

    Store module version in ovr/version.py

--- ovr/__init__.py
+++ ovr/__init__.py
@@ -1,12 +1,8 @@
 
+from .version import __version__
+
 # First try loading API 0.8.0
 try:
     from _ovr080 import *
 except:
     raise
-    # fall through to load 0.7.0
-    try:
-        from _ovr070 import *
-    except:
-        print "Do you have the Oculus Runtime installed?"
-        raise
