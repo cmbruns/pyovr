53e0bb363eb090487072583324680005b48120ff
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-08-06 08:56:43 -0400

    Git ignore eclipse project files. Modify glfw example to use glfw module like in pyopenvr samples.

--- examples/glfw/rift_demo_glfw.py
+++ examples/glfw/rift_demo_glfw.py
@@ -1,42 +1,52 @@
 
-import pyglfw.pyglfw as glfw
+import glfw
 
 from ovr.triangle_drawer_compatibility import TriangleDrawerCompatibility
 from ovr.rift_gl_renderer_compatibility import RiftGLRendererCompatibility
 
-def main():
-    # Initialize the library
-    if not glfw.init():
-        return
-    # Create a windowed mode window and its OpenGL context
-    window = glfw.window.Window(640, 480, "Hello World", None, None)
-    if not window:
+
+class GlfwApp(object):
+
+    def key_callback(self, window, key, scancode, action, mods):
+        "press ESCAPE to quit the application"
+        if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+            glfw.set_window_should_close(window, True)
+
+    def run(self):
+        # Initialize the library
+        if not glfw.init():
+            return
+        # Create a windowed mode window and its OpenGL context
+        self.window = glfw.create_window(640, 480, "Hello World", None, None)
+        if not self.window:
+            glfw.terminate()
+            return
+    
+        renderer = RiftGLRendererCompatibility()
+        # Paint a triangle in the center of the screen
+        renderer.append(TriangleDrawerCompatibility())
+    
+        # Make the window's context current
+        glfw.make_context_current(self.window)
+    
+        # Initialize Oculus Rift
+        renderer.init_gl()
+        renderer.rift.recenter_pose()
+        
+        glfw.set_key_callback(self.window, self.key_callback)
+    
+        # Loop until the user closes the window
+        while not glfw.window_should_close(self.window):
+            # Render here, e.g. using pyOpenGL
+            renderer.display_rift_gl()
+    
+            # Swap front and back buffers
+            glfw.swap_buffers(self.window)
+    
+            # Poll for and process events
+            glfw.poll_events()
+    
         glfw.terminate()
-        return
-
-    renderer = RiftGLRendererCompatibility()
-    # Paint a triangle in the center of the screen
-    renderer.append(TriangleDrawerCompatibility())
-
-    # Make the window's context current
-    window.make_current()
-
-    # Initialize Oculus Rift
-    renderer.init_gl()
-    renderer.rift.recenter_pose()
-
-    # Loop until the user closes the window
-    while not window.should_close:
-        # Render here, e.g. using pyOpenGL
-        renderer.display_rift_gl()
-
-        # Swap front and back buffers
-        window.swap_buffers()
-
-        # Poll for and process events
-        glfw.poll_events()
-
-    glfw.terminate()
 
 if __name__ == "__main__":
-    main()
+    GlfwApp().run()
