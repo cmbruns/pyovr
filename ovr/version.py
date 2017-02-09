0954fd7b46d0eb6f04dc4bd4eee04a673a713220
Cami <ckbruns@rotatingpenguin.com>
2017-02-09 12:10:36 -0500

    Updating to Oculus SDK 1.11.0

--- ovr/version.py
+++ ovr/version.py
@@ -4,5 +4,5 @@
 # 3) we can import it into your module module
 # http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '1.10.0101'
+__version__ = '1.11.0001'
 
